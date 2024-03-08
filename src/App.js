dfe2740db88112787b597f566e5236286f495193
mjh9401 <mjh9401@naver.com>
2024-03-08 14:24:20 +0900

    지출목록 추가기능

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import ExpenseLIst from "./components/ExpenseLIst";
 
 
 const App = ()=> { 
+  // state
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState(0);
   const [expense,setExpense] = useState(
     [
       {id : 1, charge:"렌트비", amount: 1600},
@@ -20,30 +23,59 @@ const App = ()=> {
     setExpense(newExpenses);
   }
 
-    return(
-      <main className="main-container">
-        <h1>예산 계산기</h1>
+  const handleChange = (e)=>{
+      console.log(e.target.value);
+      setCharge(e.target.value);
+  }
 
-        <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
-          {/* Expense Form */}
-          <ExpenseForm/>
-        </div>
+  const handleAmount =(e)=>{
+    setAmount(e.target.valueAsNumber);
+  }
 
-        <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
-          {/* Expense List */}
-          <ExpenseLIst 
-            initailExpenses={expense}
-            handleDelete={handleDelete}/>
-        </div>
-        
-        <div style={{display:'flex', justifyContent:'end', marginTop:'1rem'}}>
-          <p style={{fontSize:'2rem'}}>
-            총지출:
-            <span>원</span>
-          </p>
-        </div>
-      </main>
-    )
+  const handleSubmit = (e)=>{
+    e.preventDefault(); // submit 기본동작 막음
+    
+    if(charge !== "" && amount >0){
+        const newExpense = {id : crypto.randomUUID(), charge:charge, amount:amount};
+
+        // 불편성을 지켜주기 위해서 새로운 expenses를 생성
+        const newExpenses = [...expense, newExpense];
+        setExpense(newExpenses);
+        setCharge("");
+        setAmount(0);
+
+    }else{
+      console.log("error");
+    }
+  }
+
+  return(
+    <main className="main-container">
+      <h1>예산 계산기</h1>
+      <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
+        {/* Expense Form */}
+        <ExpenseForm 
+          handleChange = {handleChange}
+          charge = {charge}
+          handleAmout={handleAmount}
+          amount={amount}
+          handleSubmit = {handleSubmit}/>
+      </div>
+      <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
+        {/* Expense List */}
+        <ExpenseLIst 
+          initailExpenses={expense}
+          handleDelete={handleDelete}/>
+      </div>
+      
+      <div style={{display:'flex', justifyContent:'end', marginTop:'1rem'}}>
+        <p style={{fontSize:'2rem'}}>
+          총지출:
+          <span>원</span>
+        </p>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,17 +2,25 @@ import React from 'react'
 import "./ExpenseForm.css";
 import { MdSend } from 'react-icons/md';
 
-const ExpenseForm =()=>{
+const ExpenseForm =({handleChange, charge, handleAmout, amount, handleSubmit})=>{
     return (
-      <form>  
+      <form onSubmit={handleSubmit}>  
         <div className='form-center'>
           <div className='form-group'>
               <label htmlFor="charge">지출항목</label>
-              <input type="text" className='form-control' id="charge" name="charge" placeholder='예) 렌트비'/>
+              <input type="text" className='form-control' 
+                    id="charge" name="charge"
+                    onChange={handleChange}
+                    value={charge} 
+                    placeholder='예) 렌트비'/>
           </div>
           <div className='form-group'>
               <label htmlFor="amount">비용</label>
-              <input type="number" className='form-control' id="amount" name="amount" placeholder='예) 100'/>
+              <input type="number" className='form-control' 
+                      id="amount" name="amount"
+                      value={amount}
+                      onChange={handleAmout} 
+                      placeholder='예) 100'/>
           </div>
         </div>
         <button type='submit' className='btn'>
